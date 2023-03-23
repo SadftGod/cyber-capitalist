bffbf7d8a4a2daa534ba623aaa021c2db6508cef
Yurii Mytko <uri200306@gmail.com>
2023-03-24 01:35:30 +0200

    add third section

--- src/components/TelegramSection3/TelegramSection3.js
+++ src/components/TelegramSection3/TelegramSection3.js
@@ -19,6 +19,30 @@ class TelegramSection3 extends React.Component {
                   </div>
                   <p className='awaits'>Adventures awaits.</p>
                </div>
+               <div className='con__for__absolute__el'>
+                  <img className='icon__conector__tg' src='./img/icon-connection.png'/>
+                  <img className='f__conector' src='./img/f-connector.png'/>
+                  <img className='telegram__icon' src='./img/Frame 19.png'/>
+                  <div className='img__t__section'>
+                     <img className='icon__conector__img' src='./img/icon-connection.png'/>
+                     <img className='img__comp' src='./img/img-section3.png'/>
+                     <img className='img__comp__blur' src='./img/img-section3-blur.png'/>
+                  </div>
+                  <img className='s__conector' src='./img/s-connector.png'/>
+                  <img className='capital' src='./img/capital.png'/>
+                  <p className='text__capital'>your capital</p>
+                  <img className='icon__conector__capital' src='./img/icon-connection.png'/>
+                  <img className='t__conector' src='./img/t-connector.png'/>
+                  <div className='user__icon__con'>
+                     <img src='./img/user-sign.png'/>
+                     <img src='./img/user-sign.png'/>
+                     <img src='./img/user-sign.png'/>
+                     <img src='./img/user-sign.png'/>
+                     <img src='./img/user-sign.png'/>
+                     <img src='./img/user-sign.png'/>
+                  </div>
+
+               </div>
             </div>
          </div>
       )
